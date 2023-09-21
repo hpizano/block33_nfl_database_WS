b823d5392bcd2ee7fe4bf208c08d08d9dea340a6
Hector P <pizano_hector@outlook.com>
2023-09-21 08:51:50 -0500

    get, post, delete backened

--- server.js
+++ server.js
@@ -1,5 +1,69 @@
 const pg = require('pg');
+const cors = require('cors');
+const morgan = require('morgan');
 const client = new pg.Client('postgres://localhost/nfl_backend_db')
+const express = require('express');
+const app = express();
+app.use(cors());
+app.use(morgan('dev'))
+app.use(express.json())
+
+
+app.get('/api/players', async(req, res, next)=> {
+    try {
+        const SQL = `
+        SELECT * FROM players
+        `;
+        const response = await client.query(SQL);
+        res.send(response.rows);
+    }
+    catch(error){
+        next(error);
+    }
+})
+
+app.get('/api/players/:id', async(req,res,next) => {
+    try {
+        const SQL = `
+            SELECT * FROM players
+            WHERE id = $1
+        `;
+        const response = await client.query(SQL, [req.params.id]);
+        res.send(response.rows[0]);
+    } catch(error){
+        next(error);
+    };
+});
+
+app.delete('/api/players/:id', async(req, res, next) => {
+    try{
+        const SQL = `
+        DELETE FROM players
+        WHERE id = $1
+        `
+        const response = await client.query(SQL, [req.params.id])
+        res.send(response.rows);
+    } catch(error){
+        next(error);
+    }
+})
+
+app.post('/api/players', async(req,res,next) => {
+    // const body = req.body
+    try{
+        const SQL = `
+        INSERT INTO players(name, position)
+        VALUES($1, $2)
+        RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.name, req.body.position])
+        res.send(response.rows)
+    }catch(error){
+        next(error);
+    }
+})
+
+
 
 const start = async() => {
     await client.connect();
@@ -14,10 +78,13 @@ const start = async() => {
     INSERT INTO players(name, position) values('Aaron Rodgers', 'QB');
     INSERT INTO players(name, position) values('Aaron Jones', 'RB');
     INSERT INTO players(name, position) values('Patrick Mahomes', 'QB');
+    INSERT INTO players(name, position) values('Stefon Diggs', 'WR');
 
     `;
     await client.query(SQL);
     console.log('tables created')
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, ()=> console.log(`listening on port ${PORT}`));
 };
 
 start();
